bb563bf1d54924c3baef97b6737b89fa00ee50b4
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-09-28 16:33:48 -0300

    Feat: Set up basic attack animation

--- CharacterBody2D.cs
+++ CharacterBody2D.cs
@@ -3,90 +3,139 @@ using System;
 
 public partial class CharacterBody2D : Godot.CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+    public const float Speed = 300.0f;
+    public const float JumpVelocity = -400.0f;
 
     private AnimatedSprite2D sprite;
     private string animation;
     private AnimationPlayer animator;
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+    private Timer attackTimer;
+    private Timer landTimer;
+    // Get the gravity from the project settings to be synced with RigidBody nodes.
+    public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
     private bool doubleJumpAvailable = true;
-    // private bool landed = true;
-    
-    
-    public override void _Ready(){
+    private bool landed = true;
+    private bool attacking = false;
+
+
+    public override void _Ready()
+    {
         sprite = GetNode<AnimatedSprite2D>("Sprite");
         animator = GetNode<AnimationPlayer>("AnimationPlayer");
-        
+        attackTimer = GetNode<Timer>("AttackTimer");
+        landTimer = GetNode<Timer>("LandTimer");
+        attackTimer.Timeout += OnAttackTimerTimeout;
+        landTimer.Timeout += OnLandTimerTimeout;
     }
 
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector2 velocity = Velocity;
-		// Add the gravity.
-		if (!IsOnFloor()){
-			velocity.Y += gravity * (float)delta;
-            // landed = false;
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector2 velocity = Velocity;
+        // Add the gravity.
+        if (!IsOnFloor())
+        {
+            velocity.Y += gravity * (float)delta;
+            landed = false;
         }
 
         if (IsOnFloor())
         {
-            // landed = false;
+
             doubleJumpAvailable = true;
         }
 
-		// Handle Jump.
-		if ((Input.IsActionJustPressed("ui_accept") || Input.IsActionJustPressed("move_up") )&& (doubleJumpAvailable || IsOnFloor())){
+        // Handle Jump.
+        if ((Input.IsActionJustPressed("ui_accept") || Input.IsActionJustPressed("move_up")) && (doubleJumpAvailable || IsOnFloor()))
+        {
             if (!IsOnFloor())
                 doubleJumpAvailable = false;
-			velocity.Y = JumpVelocity;
+            velocity.Y = JumpVelocity;
         }
+
+        if (Input.IsActionJustPressed("attack") && IsOnFloor())
+            HandleAttack();
 
         Flip(velocity);
         Animation(velocity);
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		// Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+        // Get the input direction and handle the movement/deceleration.
+        // As good practice, you should replace UI actions with custom gameplay actions.
+        // Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
         Vector2 direction = new Vector2(
     Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left"),
     Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up")
 );
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
-		Velocity = velocity;
-		MoveAndSlide();
-	}
-
-
-private void Flip(Vector2 vector){
-    
-    if (vector.X != 0)
-        sprite.FlipH = (vector.X > 0) ? false : true;
-}
-
-private void Animation(Vector2 vector){
-    string anim;
-    if(IsOnFloor()){
-        anim = vector.X != 0 ? "run" : "idle";
-    } else {
-        anim = vector.Y > 0 ? "fall" : "jump";
+        if (direction != Vector2.Zero)
+        {
+            velocity.X = direction.X * Speed;
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+        }
+        Velocity = velocity;
+        MoveAndSlide();
     }
-    SetAnimation(anim);
-}
 
-private void SetAnimation(string anim){
-    if(animation != anim){
-        animation = anim;
-        sprite.Play(animation);
+
+    private void Flip(Vector2 vector)
+    {
+
+        if (vector.X != 0)
+            sprite.FlipH = (vector.X > 0) ? false : true;
     }
-}
-    
+
+    private void Animation(Vector2 vector)
+    {
+        string anim;
+        if (IsOnFloor())
+        {
+            if (attacking)
+            {
+                anim = "attacking";
+            }
+            else
+            {
+                anim = vector.X != 0 ? "run" : "idle";
+            }
+        }
+        else
+        {
+            anim = vector.Y > 0 ? "fall" : "jump";
+        }
+        SetAnimation(anim);
+    }
+
+    private void SetAnimation(string anim)
+    {
+        if (animation != anim)
+        {
+            animation = anim;
+            sprite.Play(animation);
+        }
+    }
+
+    private void HandleAttack()
+    {
+        attacking = true;
+        attackTimer.Start();
+    }
+
+    private void HandleLand()
+    {
+        // landed = true;
+        SetAnimation("landed");
+        landTimer.Start();
+    }
+
+    private void OnAttackTimerTimeout()
+    {
+        attacking = false;
+    }
+
+    private void OnLandTimerTimeout()
+    {
+        landed = false;
+    }
+
 }
