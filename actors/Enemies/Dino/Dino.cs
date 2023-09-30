f8508e01b9c6e2a5d322e7360b870e918422bd8a
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-09-30 11:02:07 -0300

    Feat: Basic mob movement

--- actors/Enemies/Dino/Dino.cs
+++ actors/Enemies/Dino/Dino.cs
@@ -6,26 +6,32 @@ public partial class Dino : CharacterBody2D
 
     // Get the gravity from the project settings to be synced with RigidBody nodes.
     public new float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+    public float Speed = 30.0f;
+    private Sprite2D sprite;
     private bool hit = false;
     private bool dead = false;
     private Timer hitTimer;
+    private Timer movementTimer;
     private string animation;
     private AnimationPlayer animator;
     [Export]
     public Damageable damageable;
     public override void _Ready()
     {
+        sprite = GetNode<Sprite2D>("Sprite");
         animator = GetNode<AnimationPlayer>("AnimationPlayer");
         damageable.Connect("OnHitEventHandler", new Callable(this, nameof(OnDamageableHit)));
         hitTimer = GetNode<Timer>("HitTimer");
+        movementTimer = GetNode<Timer>("MovementTimer");
         hitTimer.Timeout += OnHitTimerTimeout;
+        movementTimer.Timeout += OnMovementTimerTimeout;
+        movementTimer.Start();
     }
     private void OnDamageableHit(Node node, float ammountChanged)
     {
         if (damageable.health <= 0) dead = true;
         hit = true;
         hitTimer.Start();
-        GD.Print("OUCH!");
     }
     public override void _PhysicsProcess(double delta)
     {
@@ -35,9 +41,32 @@ public partial class Dino : CharacterBody2D
         if (!IsOnFloor())
             velocity.Y += gravity * (float)delta;
 
-        Velocity = velocity;
         Animation(velocity);
+        if (!hit) HandleMovement(velocity);
+    }
+
+    private void HandleMovement(Vector2 velocity)
+    {
+        Vector2 direction = new Vector2(-1, 0);
+        if (direction != Vector2.Zero)
+        {
+            velocity.X = direction.X * Speed;
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+        }
+        Velocity = velocity;
+        Flip(velocity);
         MoveAndSlide();
+    }
+
+    private void Flip(Vector2 vector)
+    {
+        if (vector.X != 0)
+        {
+            sprite.FlipH = vector.X > 0;
+        }
     }
 
     private void Animation(Vector2 vector)
@@ -77,6 +106,11 @@ public partial class Dino : CharacterBody2D
     private void OnHitTimerTimeout()
     {
         hit = false;
+    }
+
+    private void OnMovementTimerTimeout()
+    {
+        Speed = -Speed;
     }
 
 }
